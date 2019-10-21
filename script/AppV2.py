0ef12ec772cc406a90541f63c42e866b88bd70c2
PierreNat <pierre.cuony@students.unibe.ch>
2019-10-21 16:11:59 +0200

    update not ideal

--- script/AppV2.py
+++ script/AppV2.py
@@ -4,10 +4,6 @@ import os
 
 import tkinter as tk
 
-Global_pos_x = 0
-Global_pos_y = 0
-
-
 class CommandWindow:
     def __init__(self, master):
         self.master = master
@@ -16,6 +12,7 @@ class CommandWindow:
         self.button2 = tk.Button(self.frame, text = 'close', width = 25, command = self.close_image)
         self.button3 = tk.Button(self.frame, text = 'Next', width = 25, command = self.next_frame)
         self.button4 = tk.Button(self.frame, text = 'Previous', width = 25, command = self.prev_frame)
+        self.frame.bind('<Motion>', self.motion)
         self.button1.grid(row=0, column=0)
         self.button2.grid(row=1, column=0)
         self.button3.grid(row=2, column=0)
@@ -24,26 +21,34 @@ class CommandWindow:
         self.currentImage = 0 #contain the frame number to pick in the set
         self.span = 10 # jump between frames to see the tool moving
         self.number_frame = 0 # diplayed frames count
-        self.current_cursor_pos_X =0
-        self.current_cursor_pos_Y  =0
-        self.label_1 = Label(self.frame, text='Position red 1')
-        string_to_display = Global_pos_x
-        var1 = StringVar()
-        var1.set(string_to_display)
-        self.entry_1 = Entry(self.frame, textvariable = var1)
-
-        self.label_1.grid(row=5, column=0)
-        self.entry_1.grid(row=5, column=1)
-
+        self.current_cursor_pos_X = 0
+        self.current_cursor_pos_Y  = 0
+        self.LabelRed1_x = Label(self.frame, text='Red 1 x pos')
+        self.LabelRed1_y = Label(self.frame, text='Red 1 y pos')
+        self.string_to_display = 10
+        self.Red1_x = StringVar()
+        self.Red1_x .set(self.string_to_display)
+        self.Red1_y = StringVar()
+        self.Red1_y .set(self.string_to_display)
+        self.entry_Red1_x = Entry(self.frame, textvariable = self.Red1_x)
+        self.entry_Red1_y = Entry(self.frame, textvariable = self.Red1_y)
+        self.LabelRed1_x .grid(row=5, column=0)
+        self.entry_Red1_x .grid(row=5, column=1)
+        self.LabelRed1_y .grid(row=5, column=3)
+        self.entry_Red1_y .grid(row=5, column=4)
         # self.imWindow = tk.Toplevel(self.master)
         # self.img = ImageTk.PhotoImage(Image.open("framesLeft/frameL{}.jpg".format(0)))
 
     def new_window(self):
         self.newWindow = tk.Toplevel(self.master)
-        self.app = Demo2(self.newWindow, ImageId=self.currentImage)
+        self.app = Child_window(self.newWindow, ImageId=self.currentImage)
 
     def close_image(self):
         self.app.close_windows()
+
+    # def get_coordinate(self):
+    #     self.current_cursor_pos_X, self.current_cursor_pos_Y = self.app.
+    #     print(self.current_cursor_pos_X, self.current_cursor_pos_Y)
 
     def next_frame(self):
         if (self.currentImage+self.span < 18000):
@@ -62,9 +67,22 @@ class CommandWindow:
         self.close_image()
         self.new_window()
 
-class Demo2:
+
+    def motion(self,event):
+        if self.app.clk:
+            print('{}, {}'.format(self.app.x, self.app.y))
+            self.app.clk = False
+        self.Red1_x .set(self.app.x)
+        self.entry_Red1_x = Entry(self.frame, textvariable = self.Red1_x)
+        self.Red1_y .set(self.app.y)
+        self.entry_Red1_y = Entry(self.frame, textvariable = self.Red1_y)
+
+class Child_window:
     def __init__(self, master, ImageId=0):
         self.master = master
+        self.x = 0
+        self.y =0
+        self.clk = False
         # master.minsize(width=1280, height=1024)
         self.frame = tk.Frame(self.master)
         self.master.title("Frame number {}".format(ImageId))
@@ -74,11 +92,28 @@ class Demo2:
         print( imageinfo.size[0], imageinfo.size[1] )
         self.canvas.create_image(0,0, image=self.img, anchor="nw")
         self.canvas.pack()
-        master.bind('<Button-1>', leftClick)
-
+        master.bind('<Button-1>', self.clicked)
+        # master.bind('<Motion>', self.motion)
+        # print(self.mm.pos_x)
+        # print(self.mm.pos_y)
+        # master.bind('<Button-1>', leftClick)
 
     def close_windows(self):
         self.master.destroy()
+
+
+    def clicked(self, event):
+        self.x, self.y = event.x, event.y
+        self.clk = True
+        # print(x,y)
+
+    # def motion(self,event):
+    #     if self.clicked:
+    #         print('{}, {}'.format(self.x, self.y))
+    #         self.clicked = False
+
+
+
 
     # def next_frame(self):
     #     self.master.destroy()
@@ -88,18 +123,32 @@ class Demo2:
     #     self.canvas.create_image(0,0, image=self.img, anchor="nw")
     #     self.canvas.pack()
 
-def leftClick(event):
-    print("left")
-    print('pos x {}'.format(event.x))
-    print('pos y {}'.format(event.y))
-    Global_pos_x = event.x
-    Global_pos_y = event.y
+# def leftClick(event):
+#     print("left")
+#     print('pos x {}'.format(event.x))
+#     print('pos y {}'.format(event.y))
+#     Global_pos_x = event.x
+#     Global_pos_y = event.y
     # return event.x, event.y
+
+
+class MousePose():
+  def __init__(self):
+    self.pos_x = 0
+    self.pos_y = 0
+
+
+  def select(self, event):
+      print("left")
+      pos_x  = event.x
+      pos_y  = event.y
 
 
 def main():
     root = tk.Tk()
     app = CommandWindow(root)
+    # Get an instance of the MousePos object
+
     root.mainloop()
 
 if __name__ == '__main__':
