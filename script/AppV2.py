2df163b2997928888060a131fd61ae3d757d9ef6
PierreNat <pierre.cuony@students.unibe.ch>
2019-10-21 12:16:00 +0200

    click value dont change, no update of the global variable, seems not the best idea

--- script/AppV2.py
+++ script/AppV2.py
@@ -4,6 +4,10 @@ import os
 
 import tkinter as tk
 
+Global_pos_x = 0
+Global_pos_y = 0
+
+
 class CommandWindow:
     def __init__(self, master):
         self.master = master
@@ -12,13 +16,25 @@ class CommandWindow:
         self.button2 = tk.Button(self.frame, text = 'close', width = 25, command = self.close_image)
         self.button3 = tk.Button(self.frame, text = 'Next', width = 25, command = self.next_frame)
         self.button4 = tk.Button(self.frame, text = 'Previous', width = 25, command = self.prev_frame)
-        self.button1.pack()
-        self.button2.pack()
-        self.button3.pack()
-        self.button4.pack()
+        self.button1.grid(row=0, column=0)
+        self.button2.grid(row=1, column=0)
+        self.button3.grid(row=2, column=0)
+        self.button4.grid(row=3, column=0)
         self.frame.pack()
-        self.currentImage = 10000
-        self.span = 10
+        self.currentImage = 0 #contain the frame number to pick in the set
+        self.span = 10 # jump between frames to see the tool moving
+        self.number_frame = 0 # diplayed frames count
+        self.current_cursor_pos_X =0
+        self.current_cursor_pos_Y  =0
+        self.label_1 = Label(self.frame, text='Position red 1')
+        string_to_display = Global_pos_x
+        var1 = StringVar()
+        var1.set(string_to_display)
+        self.entry_1 = Entry(self.frame, textvariable = var1)
+
+        self.label_1.grid(row=5, column=0)
+        self.entry_1.grid(row=5, column=1)
+
         # self.imWindow = tk.Toplevel(self.master)
         # self.img = ImageTk.PhotoImage(Image.open("framesLeft/frameL{}.jpg".format(0)))
 
@@ -36,6 +52,7 @@ class CommandWindow:
             self.currentImage = self.currentImage
         self.close_image()
         self.new_window()
+        self.number_frame = self.number_frame+1
 
     def prev_frame(self):
         if (self.currentImage-self.span > 0):
@@ -50,23 +67,35 @@ class Demo2:
         self.master = master
         # master.minsize(width=1280, height=1024)
         self.frame = tk.Frame(self.master)
+        self.master.title("Frame number {}".format(ImageId))
         imageinfo = Image.open("framesLeft/frameL{}.jpg".format(ImageId))
         self.img = ImageTk.PhotoImage(Image.open("framesLeft/frameL{}.jpg".format(ImageId)))
         self.canvas = Canvas(self.master, width = imageinfo.size[0], height = imageinfo.size[1])
+        print( imageinfo.size[0], imageinfo.size[1] )
         self.canvas.create_image(0,0, image=self.img, anchor="nw")
         self.canvas.pack()
+        master.bind('<Button-1>', leftClick)
 
 
     def close_windows(self):
         self.master.destroy()
 
-    def next_frame(self):
-        self.master.destroy()
-        imageinfo = Image.open("framesLeft/frameL{}.jpg".format(10))
-        self.img = ImageTk.PhotoImage(Image.open("framesLeft/frameL{}.jpg".format(10)))
-        self.canvas = Canvas(self.master, width = imageinfo.size[0], height = imageinfo.size[1])
-        self.canvas.create_image(0,0, image=self.img, anchor="nw")
-        self.canvas.pack()
+    # def next_frame(self):
+    #     self.master.destroy()
+    #     imageinfo = Image.open("framesLeft/frameL{}.jpg".format(10))
+    #     self.img = ImageTk.PhotoImage(Image.open("framesLeft/frameL{}.jpg".format(10)))
+    #     self.canvas = Canvas(self.master, width = imageinfo.size[0], height = imageinfo.size[1])
+    #     self.canvas.create_image(0,0, image=self.img, anchor="nw")
+    #     self.canvas.pack()
+
+def leftClick(event):
+    print("left")
+    print('pos x {}'.format(event.x))
+    print('pos y {}'.format(event.y))
+    Global_pos_x = event.x
+    Global_pos_y = event.y
+    # return event.x, event.y
+
 
 def main():
     root = tk.Tk()
