3cf117e13539aa46926d1f2b4c764762ecab17a6
PierreNat <pierre.cuony@students.unibe.ch>
2019-11-04 21:54:32 +0100

    first render test is weird

--- script/AppV3_SixPointsV2.py
+++ script/AppV3_SixPointsV2.py
@@ -8,7 +8,10 @@ from functools import partial
 import tkinter as tk
 import cv2
 import numpy as np
-
+import neural_renderer as nr
+import torch
+from utils_functions.camera_settings import camera_setttings
+import matplotlib.pyplot as plt
 
 ##### PARAMETERS GO HERE ###########
 shaft_diameter = 8.25*1e-3
@@ -750,7 +753,79 @@ class CommandWindow:
     def renderingGivenTm(self):
         print('rendering the 3D cad tool')
 
+        instrument_to_camera_transform= self.T_m
 
+        Extracted_theta3_rad = math.atan2(instrument_to_camera_transform[1, 0], instrument_to_camera_transform[0, 0])
+        C_2 = math.sqrt(instrument_to_camera_transform[2, 1] * instrument_to_camera_transform[2, 1] +
+                     instrument_to_camera_transform[2, 2] * instrument_to_camera_transform[2, 2])
+        Extracted_theta2_rad = math.atan2(-instrument_to_camera_transform[2, 0], C_2)
+        Extracted_theta1_rad = math.atan2(instrument_to_camera_transform[2, 1], instrument_to_camera_transform[2, 2])
+
+        Extracted_X = instrument_to_camera_transform[0, 3]
+        Extracted_Y = instrument_to_camera_transform[1, 3]
+        Extracted_Z = instrument_to_camera_transform[2, 3]
+
+        Extracted_theta1_deg = np.degrees(Extracted_theta1_rad)
+        Extracted_theta2_deg = np.degrees(Extracted_theta2_rad)
+        Extracted_theta3_deg = np.degrees(Extracted_theta3_rad)
+
+        # define transfomration parameter from json file
+        alpha = Extracted_theta1_deg
+        beta = Extracted_theta2_deg
+        gamma = Extracted_theta3_deg
+        x = Extracted_X
+        y = Extracted_Y
+        z = Extracted_Z
+        print('parameter found are: ',x, y, z, alpha, beta, gamma)
+        vertices_1, faces_1, textures_1 = nr.load_obj("3D_objects/shaft.obj", load_texture=True)  # , texture_size=4)
+        vertices_1 = vertices_1[None, :, :]  # add dimension
+        faces_1 = faces_1[None, :, :]  # add dimension
+        textures_1 = textures_1[None, :, :]  # add dimension
+        nb_vertices = vertices_1.shape[0]
+
+        R = np.array([np.radians(alpha), np.radians(beta), np.radians(gamma)])  # angle in degree
+        t = np.array([x, y, z])  # translation in meter
+
+        # define transformation by transformation matrix
+
+        Rt = np.concatenate((R, t), axis=None).astype(np.float16)  # create one array of parameter in radian, this arraz will be saved in .npy file
+
+        cam = camera_setttings(R=R, t=t, vert=nb_vertices, resolutionx=1280, resolutiony=1024,cx=590, cy=508, fx=1067, fy=1067) # degree angle will be converted  and stored in radian
+
+        renderer = nr.Renderer(image_size=1280, camera_mode='projection', dist_coeffs=None,
+                               K=cam.K_vertices, R=cam.R_vertices, t=cam.t_vertices, near=0.01, background_color=[1, 1, 1],
+                               # background is filled now with  value 0-1 instead of 0-255
+                               # changed from 0-255 to 0-1
+                               far=10, orig_size=1280,
+                               light_intensity_ambient=1, light_intensity_directional=1, light_direction=[0, 1, 0],
+                               light_color_ambient=[1, 1, 1], light_color_directional=[1, 1, 1])
+
+        images_1 = renderer(vertices_1, faces_1, textures_1,
+                            K=torch.cuda.FloatTensor(cam.K_vertices),
+                            R=torch.cuda.FloatTensor(cam.R_vertices),
+                            t=torch.cuda.FloatTensor(cam.t_vertices))  # [batch_size, RGB, image_size, image_size]
+
+        image = images_1[0].detach().cpu().numpy()[0].transpose((1, 2, 0)) #float32 from 0-1
+        image = (image*255).astype(np.uint8) #cast from float32 255.0 to 255 uint8
+
+        sils_1 = renderer(vertices_1, faces_1, textures_1,
+                          mode='silhouettes',
+                          K=torch.cuda.FloatTensor(cam.K_vertices),
+                          R=torch.cuda.FloatTensor(cam.R_vertices),
+                          t=torch.cuda.FloatTensor(cam.t_vertices))  # [batch_size, RGB, image_size, image_size]
+
+        sil = sils_1.detach().cpu().numpy().transpose((1, 2, 0))
+        sil = np.squeeze((sil * 255)).astype(np.uint8) # change from float 0-1 [512,512,1] to uint8 0-255 [512,512]
+
+        fig = plt.figure()
+        fig.add_subplot(2, 1, 1)
+        plt.imshow(image)
+        # imageio.imwrite("3D_objects/{}_ref.png".format(file_name_extension), image)
+
+        fig.add_subplot(2, 1, 2)
+        plt.imshow(sil, cmap='gray')
+        plt.show()
+        # plt.close(fig)
 
 class Child_window:
     def __init__(self, master, ImageId=0):
--- script/RendererWithrealCamera.py
+++ script/RendererWithrealCamera.py
@@ -37,21 +37,69 @@ def main():
 
 
     nb_im = 1
-
+    useTransformMatrix = True
+    instrument_to_camera_transform = np.array([(0, 0, 0, 0),
+                                               (0, 0, 0, 0),
+                                               (0, 0, 0, 0),
+                                               (0, 0, 0, 1)])
+    instrument_to_camera_transform = np.zeros((4,4))
 
     ## --------------------------------------------------------------------------------
 
     loop = tqdm.tqdm(range(0, nb_im))
     for i in loop:
 
-        # # define transfomration parameter from angle and translation
 
-        alpha =0#uniform(0, 180)
-        beta = 90#uniform(0, 180)
-        gamma =  0 #uniform(0, 180)
-        x = 0.00 #uniform(-1.5, 1.5)
-        y = 0 #uniform(-1.5, 1.5)
-        z = 0.9 #uniform(5, 7) #1000t was done with value between 7 and 10, Rot and trans between 5 10
+
+
+        # # define transfomration parameter from angle and translation
+        if useTransformMatrix :
+            R_test = np.array([np.radians(145), np.radians(5), np.radians(-89)])  # test value alpha beta gamma
+            T_test = np.array([-0.007, 0.008, 0.08])
+            T_test_vector, R_test_matrix = BuildTransformationMatrix(tx=T_test[0], ty=T_test[1], tz=T_test[2], alpha=R_test[0], beta=R_test[1],
+                                                                     gamma=R_test[2]) #return 1x3 vector and 3x3matrix
+            instrument_to_camera_transform[0, 0:3] = R_test_matrix[0, :]
+            instrument_to_camera_transform[1, 0:3] = R_test_matrix[1, :]
+            instrument_to_camera_transform[2, 0:3] = R_test_matrix[2, :]
+            instrument_to_camera_transform[0, 3] = T_test[0]
+            instrument_to_camera_transform[1, 3] = T_test[1]
+            instrument_to_camera_transform[2, 3] = T_test[2]
+            instrument_to_camera_transform[3, 3] = 1
+            # test[0:3,0:3] = R_test_matrix
+
+            # formula from http://planning.cs.uiuc.edu/node103.html
+            # alpha and gamma were swapped, don-t know where the problem is but value are correct
+            Extracted_theta3_rad = m.atan2(instrument_to_camera_transform[1, 0], instrument_to_camera_transform[0, 0])
+            C_2 = m.sqrt(instrument_to_camera_transform[2, 1] * instrument_to_camera_transform[2, 1] +
+                         instrument_to_camera_transform[2, 2] * instrument_to_camera_transform[2, 2])
+            Extracted_theta2_rad = m.atan2(-instrument_to_camera_transform[2, 0], C_2)
+            Extracted_theta1_rad = m.atan2(instrument_to_camera_transform[2, 1], instrument_to_camera_transform[2, 2])
+
+
+            Extracted_X = instrument_to_camera_transform[0, 3]
+            Extracted_Y = instrument_to_camera_transform[1, 3]
+            Extracted_Z = instrument_to_camera_transform[2, 3]
+
+            Extracted_theta1_deg = np.degrees(Extracted_theta1_rad)
+            Extracted_theta2_deg = np.degrees(Extracted_theta2_rad)
+            Extracted_theta3_deg = np.degrees(Extracted_theta3_rad)
+
+            # define transfomration parameter from json file
+            alpha =Extracted_theta1_deg
+            beta = Extracted_theta2_deg
+            gamma =  Extracted_theta3_deg
+            x = Extracted_X
+            y = Extracted_Y
+            z = Extracted_Z
+            print(x,y,z, alpha,beta,gamma)
+
+        else:
+            alpha =0#uniform(0, 180)
+            beta = 90#uniform(0, 180)
+            gamma =  0 #uniform(0, 180)
+            x = 0.00 #uniform(-1.5, 1.5)
+            y = 0 #uniform(-1.5, 1.5)
+            z = 0.9 #uniform(5, 7) #1000t was done with value between 7 and 10, Rot and trans between 5 10
 
 
 
@@ -65,7 +113,7 @@ def main():
         cam = camera_setttings(R=R, t=t, vert=nb_vertices, resolutionx=1280, resolutiony=1024,cx=590, cy=508, fx=1067, fy=1067) # degree angle will be converted  and stored in radian
 
         renderer = nr.Renderer(image_size=1280, camera_mode='projection', dist_coeffs=None,
-                               K=cam.K_vertices, R=cam.R_vertices, t=cam.t_vertices, near=0.01, background_color=[1, 1, 1],
+                               K=cam.K_vertices, R=cam.R_vertices, t=cam.t_vertices, near=0.001, background_color=[1, 1, 1],
                                # background is filled now with  value 0-1 instead of 0-255
                                # changed from 0-255 to 0-1
                                far=10, orig_size=1280,
